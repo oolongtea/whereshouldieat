5fb3482ec36ea6bcf84b840fe0a629e8bfb51ee3
root <root@food.(none)>
2014-02-08 06:13:31 +0000

    second commit, POST causes 422 error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,23 @@
 class HomeController < ApplicationController
+
   def index
+  end
+
+  def sms_reply
+
+    render params
+
+    @suggestion = "Cook food you lazy!"
+    
+    client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+
+    client.account.sms.messages.create(
+      from: TWILIO_CONFIG['from'],
+      to: params['From'],
+      body: @suggestion
+    )
+
+
+    #render 'sms_reply.xml.erb', :content_type => 'text/xml'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Myapp::Application.routes.draw do
-  get "home/index"
+
+  root to: 'home#index'
+  post '/hungry' => 'home#sms_reply'
+
+#  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root to: 'home#index'
+#root to: 'home#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
