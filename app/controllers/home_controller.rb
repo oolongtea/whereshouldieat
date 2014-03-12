00b060174591612b18a7f4ef2a67399c6199d03f
oolongtea <obedespina@gmail.com>
2014-03-11 22:28:00 -0400

    add comments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,14 +10,18 @@ class HomeController < ApplicationController
     access_token = TWILIO_CONFIG[:google_token]
     radius = 500;
 
+    # follow this if user entered ACTION, ZIPCODE
     if params['Body'].split(",").size == 2 
-#&& params['Body'].split(",")[1].size == 5
       action, zipcode = params['Body'].split(",").map(&:strip)
 
+      # compile appropriate query depending on action
       query = 'restaurants+in+' + zipcode if action.downcase == 'hungry'
       query = 'bars+in+' + zipcode if action.downcase == 'thirsty'
         @suggestion = 'You gotta do it like dis: "hungry, CITY, STATE" or "thirsty, CITY, STATE"' if action.downcase != 'thirsty' && action.downcase != 'hungry'
+    
+    # follow this if user entered ACTION, CITY, STATE    
     else
+      # parse user request if correct usage
       if params['Body'].split(",").size == 3
         action, city, state = params['Body'].split(",").map(&:strip)
         city = city.gsub(' ', '+') #if city.nil?
@@ -26,6 +30,7 @@ class HomeController < ApplicationController
        action = 'h'
       end
 
+      # compile appropriate query depending on action
       if action.downcase == 'hungry'
         query = 'restaurants+in+'+city.to_s+'+'+state.to_s
       elsif action.downcase == 'thirsty'
@@ -35,10 +40,13 @@ class HomeController < ApplicationController
       end
     end
     
+    # create the Google Maps URL
     fullURL = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s&radius=%d&key=%s&sensor=false' % [query, radius, access_token]
 
+    # fetch results from Google Maps
     contents = URI.parse(fullURL).read
 
+    # parse JSON data from Google API
     c = JSON.parse(contents)
 
       if c['status'] == 'OK'
